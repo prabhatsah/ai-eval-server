76903058b083ba715f2300e39ce4ff951e9f823a
Prabhat Kumar <prabhatsah349@gmail.com>
2026-05-02 18:48:41 +0530

    setup pre-commit hook

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,7 +19,6 @@ export default tseslint.config(
       },
       sourceType: 'commonjs',
       parserOptions: {
-        project: './tsconfig.json',
         projectService: true,
         tsconfigRootDir: import.meta.dirname,
       },
