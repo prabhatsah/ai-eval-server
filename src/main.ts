64be273b3010452b21ea74b3a70b1e02d00f1e69
root <root@Prabhat-Sah.>
2026-05-11 16:32:45 +0000

    updated prod script and ports

--- src/main.ts
+++ src/main.ts
@@ -36,6 +36,10 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(process.env.PORT ?? 8080);
+  const port = process.env.PORT || 8080;
+
+  await app.listen(port, '0.0.0.0');
+
+  console.log(`Server running on port ${port}`);
 }
 bootstrap();
