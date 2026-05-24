40485ce7e707261459d52e0ffa5f9d0ec0d3d995
root <root@Prabhat-Sah.>
2026-05-24 07:11:35 +0000

    added health route

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,13 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  checkHealth() {
+    return {
+      success: true,
+      service: 'api-server',
+      status: 'healthy',
+      uptime: process.uptime(),
+      timestamp: new Date().toISOString(),
+    };
   }
 }
